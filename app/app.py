492c5f16624f4ebb8fd1a780b85d79f19263c1f5
Mihaaaela <mihaela.tabaranu24@gmail.com>
2025-12-16 14:50:15 +0200

    Improve input handling for hello endpoint

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request
+from markupsafe import escape
 
 app = Flask(__name__)
 
-# VULNERABILITATE INTENȚIONATĂ (pentru CodeQL)
+
 @app.route("/hello")
 def hello():
-    name = request.args.get("name")
-    return "Hello " + name
+    user_name = request.args.get("name")
+
+    if not user_name:
+        return "Hello!", 200
+
+    safe_name = escape(user_name)
+    return f"Hello {safe_name}", 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
